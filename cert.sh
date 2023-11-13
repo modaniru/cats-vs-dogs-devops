c90a6f909d04352830b7754e405ce1072826ad46
aleksejmoksancev <mokshantsev.aleksei>
2023-11-13 23:32:15 +0500

    test

--- cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --nginx
-sudo certbot renew --dry-run
--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git clone https://github.com/modaniru/cats-vs-dogs
 git clone https://github.com/modaniru/cats-vs-dogs-frontend
+curl -L https://raw.githubusercontent.com/wmnnd/nginx-certbot/master/init-letsencrypt.sh > init-letsencrypt.sh
+# then edit domain and email
+#chmod +x init-letsencrypt.sh
+#sudo ./init-letsencrypt.sh
